d31da3d0d5caa11f47fd64bf1f9b7769d4c39478
Sakib <sakibshaikh2425@gmail.com>
2024-11-09 23:17:03 +0530

    Added Pug Templating Engine

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found !</h1>');
+    //res.status(404).send('<h1>Page Not Found !</h1>');
+    res.status(404).render('404');
 })
 
 app.listen(3000);
