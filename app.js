3bc4918659e1c648b3db92d2c0993dce0dd6432c
Sakib <sakibshaikh2425@gmail.com>
2024-11-10 00:35:00 +0530

    Added EJS Templating Engine

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');      
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,7 +18,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     //res.status(404).send('<h1>Page Not Found !</h1>');
-    res.status(404).render('404');
+    res.status(404).render('404', { pageTitle: "Page Not Found!" });
 })
 
 app.listen(3000);
