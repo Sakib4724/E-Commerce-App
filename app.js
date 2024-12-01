43409d57619cee5a13198264261e13c931f3edd0
Sakib <sakibshaikh2425@gmail.com>
2024-12-01 22:06:55 +0530

    Implement MVC by adding productsController and errorController

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const path = require('path');
 
 const app = express();
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const errorController = require('./controllers/error');
 
 app.set('view engine', 'ejs');      
 app.set('views', 'views');
@@ -13,12 +15,9 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    //res.status(404).send('<h1>Page Not Found !</h1>');
-    res.status(404).render('404', { pageTitle: "Page Not Found!" });
-})
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,23 +2,8 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
-const adminData = require('./admin');
-
-router.get('/', (req, res, next) => {
-    // console.log(adminData.products);
-    // res.sendFile(path.join(rootDir, "views", "shop.html"));
-
-    const products = adminData.products;
-    res.render('shop', { 
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: products.length > 0, 
-        activeShop: true, 
-        productCSS: true 
-    });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
